7beb54f170535d95cd86ebbfa182c18079a2a59b
izan8259 <60820542+izanhuang@users.noreply.github.com>
2021-06-08 16:25:54 -0400

    Added Dark-Mode Toggle

--- app.js
+++ app.js
@@ -14,5 +14,17 @@ arrows.forEach((arrow,i) => {
             movieLists[i].style.transform = `translateX(0)`;
             clickCounter = 0;
         }
+    });
+});
+
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(
+    ".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle"
+);
+
+ball.addEventListener("click",()=>{
+    items.forEach(item=>{
+        item.classList.toggle("active")
     })
+    ball.classList.toggle("active")
 })
