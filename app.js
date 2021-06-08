f06487c8148a4838087070f73c2072a4f16a1f7c
izan8259 <60820542+izanhuang@users.noreply.github.com>
2021-06-08 16:01:51 -0400

    Added main content

---
+++ app.js
@@ -0,0 +1,18 @@
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrow,i) => {
+    const itemNumber = movieLists[i].querySelectorAll("img").length; // 7
+    let clickCounter = 0;
+    arrow.addEventListener("click",()=>{
+        clickCounter++;
+        if(itemNumber - (2+clickCounter) > 0){ // 7-5, 7-6, 7-7
+            movieLists[i].style.transform = `translateX(${
+                movieLists[i].computedStyleMap().get("transform")[0].x.value
+            -300}px)`;
+        } else {
+            movieLists[i].style.transform = `translateX(0)`;
+            clickCounter = 0;
+        }
+    })
+})
